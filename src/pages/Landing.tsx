f0b62ab94e93a5ed81d042556160b380e3717808
gmtommasini <gmtommasini@gmail.com>
2023-06-02 09:16:59 -0400

    update date

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import 'utils/Dates';
+// import * as React from 'react';
 import { PublishedDate } from 'utils/Dates';
 
 
@@ -7,16 +6,20 @@ export default function Landing() {
 
 
     return (
-        <>
-            <h1>Landing page</h1>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: 'calc(100vh - 80px)', justifyContent: 'space-between' }}>
+            <div>
+                <h1>Landing page</h1>
 
-            This is a page I created to study a bit of React and also expose my little backend projects.
-            <br/>
-            It is a continuous work and I'll try to keep updating and including more little projects.
-            
-            <PublishedDate date={new Date()}/>
+                This is a page I created to study a bit of React and also expose my little backend projects.
+                <br />
+                It is a continuous work and I'll try to keep updating and including more little projects.
+                
+            </div>
 
-        </>
+            {/* For now I am updating this date manually */}
+            <PublishedDate date={new Date("2023-6-2")} />
+
+        </div>
     );
 }
 
--- src/utils/Dates.tsx
+++ src/utils/Dates.tsx
@@ -14,7 +14,7 @@ interface fProps extends Props {
  * @returns string date YYYY-MM-DD
  */
 export function FormatedDate({ date, separator }: fProps): string {
-  const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "2-digit", day: "numeric" };
+  const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "2-digit", day: "2-digit" };
   let formattedDate = new Intl.DateTimeFormat("en-CA", options).format(date);
   if (separator) {
     formattedDate = formattedDate.replace(/-/g, separator);
@@ -25,6 +25,9 @@ export function FormatedDate({ date, separator }: fProps): string {
 // For the components
 let styles: React.CSSProperties = {
   fontSize: '11px',
+  textAlign: 'right',
+  margin: '10px',
+  marginBottom: '0'
 }
 export const PublishedDate: React.FC<Props> = ({ date }: Props) => {
   const pdate = FormatedDate({date});
