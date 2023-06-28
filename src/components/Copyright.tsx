2b7a2eb8de698eb24a98f06be9d1cb08356a9981
gmtommasini <gmtommasini@gmail.com>
2023-06-27 20:04:23 -0400

    copyright link address

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -7,7 +7,7 @@ export default function Copyright(props: any) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
             {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">
+            <Link color="inherit" href="https://github.com/gmtommasini/my-page" target='_blank'>
                 Guilherme 
             </Link>{' '}
             {new Date().getFullYear()}
