f044ba4e5138f4191d4e17f9ee1586ccdfc02018
gmtommasini <gmtommasini@gmail.com>
2023-07-24 17:18:57 -0400

    config

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    BUSINESS_CARD_URL : process.env.REACT_APP_BUSINESS_CARD_URL,
+    QR_CODE_URL : process.env.REACT_APP_QR_CODE_URL,
+    MUSIC_BASE_URL : process.env.REACT_APP_MUSIC_BASE_URL,
+}
+export default config;
