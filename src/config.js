e33d9eeacf71fd80b458c482e78390b0c41af30e
gmtommasini <gmtommasini@gmail.com>
2023-08-08 16:46:04 -0400

    change to REACT_APP_BACKEND_BASE_URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    BUSINESS_CARD_URL : process.env.REACT_APP_BUSINESS_CARD_URL,
-    QR_CODE_URL : process.env.REACT_APP_QR_CODE_URL,
-    MUSIC_BASE_URL : process.env.REACT_APP_MUSIC_BASE_URL,
+    BUSINESS_CARD_URL : process.env.REACT_APP_BACKEND_BASE_URL,
+    QR_CODE_URL : process.env.REACT_APP_BACKEND_BASE_URL + '/qrcode',
+    MUSIC_BASE_URL : process.env.REACT_APP_BACKEND_BASE_URL + '/api/music',
 }
 export default config;
