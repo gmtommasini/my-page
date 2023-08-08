d7ca36773719290e7db63ef94cd806dcf83f29e4
gmtommasini <gmtommasini@gmail.com>
2023-08-08 16:45:30 -0400

    removing hard code

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -1,5 +1,9 @@
 import IFrame from "components/create_iframe";
+import config from "config"; 
 
 export default function Card() {
-    return <IFrame url='https://gmtommasini.pythonanywhere.com' />;
+    if (config.BUSINESS_CARD_URL) {
+        return <IFrame url={config.BUSINESS_CARD_URL} />;        
+    } 
+    return <p>URL not found!</p>
 }
