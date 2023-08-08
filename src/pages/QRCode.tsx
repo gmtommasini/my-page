47c91e7e8349ff9978ea49c5e861964ac227d9e9
gmtommasini <gmtommasini@gmail.com>
2023-08-08 16:45:46 -0400

    cleaning

--- src/pages/QRCode.tsx
+++ src/pages/QRCode.tsx
@@ -3,9 +3,8 @@ import IFrame from "components/create_iframe";
 import config from "config"; 
 
 export default function QRCode() {
-    // return <IFrame url='https://gmtommasini.pythonanywhere.com/qrcode' />;
     if (config.QR_CODE_URL) {
-        return <IFrame url={config.QR_CODE_URL!} />;        
+        return <IFrame url={config.QR_CODE_URL} />;
     } 
     return <p>URL not found!</p>
 }
