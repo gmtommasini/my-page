65daada38d4a2923acbc7eb986dc40916e9bfcb1
gmtommasini <gmtommasini@gmail.com>
2023-08-14 14:19:38 -0400

    REACT_APP_PYTHONANYWHERE_BASE_URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    BUSINESS_CARD_URL : process.env.REACT_APP_BACKEND_BASE_URL,
-    QR_CODE_URL : process.env.REACT_APP_BACKEND_BASE_URL + '/qrcode',
+    BUSINESS_CARD_URL : process.env.REACT_APP_PYTHONANYWHERE_BASE_URL,
+    QR_CODE_URL : process.env.REACT_APP_PYTHONANYWHERE_BASE_URL + '/qrcode',
     MUSIC_BASE_URL : process.env.REACT_APP_BACKEND_BASE_URL + '/api/music',
 }
 export default config;
